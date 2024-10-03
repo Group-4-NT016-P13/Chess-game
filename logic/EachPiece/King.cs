3191d92beb515654edcab30f5f03bf2991fedf5b
khongbietcode <23521383@gm.edu.uit.vn>
2024-10-03 18:19:48 +0700

    add source code logic chess

---
+++ logic/EachPiece/King.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class King : Pieces
+    {
+        public override PieceType type => PieceType.King;
+        public override Player Color { get; }
+
+        private static readonly Direction[] dirs = new Direction[]
+        {
+            Direction.East,
+            Direction.North,
+            Direction.South,
+            Direction.West,
+            Direction.NorthEast,
+            Direction.SouthEast,
+            Direction.NorthWest,
+            Direction.SouthWest
+        };
+        public King(Player color)
+        {
+            Color = color;
+        }
+        public override Pieces copy()
+        {
+            King copy = new King(Color);
+            copy.Move = Move;
+            return copy;
+        }
+
+        private IEnumerable<Position> MovePos(Position from, Board board)
+        {
+            foreach ( Direction dir in dirs)
+            {
+                Position to= from+ dir;
+                if(!Board.Inside(to))
+                {
+                    continue;
+                }
+                if(board.Empty(to) || board[to].Color != Color)
+                {
+                    yield return to;
+                }
+            }
+        }
+        public override IEnumerable<Base> Get(Position from , Board board)
+        {
+            foreach ( Position to in MovePos(from,board))
+            {
+                yield return new Normal(from, to);
+            }
+        }
+
+        public override bool CanCaptureKing(Position from, Board board)
+        {
+            return MovePos(from, board).Any(to =>
+            {
+                Pieces piece = board[to];
+                return piece != null && piece.type == PieceType.King;
+            });
+        }
+    }
+}
---
+++ logic/EachPiece/Pawn.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logic
+{
+    public class Pawn : Pieces
+    {
+        public override PieceType type => PieceType.Pawn;
+        public override Player Color { get; }
+        private readonly Direction forward;
+        public Pawn(Player color)
+        { 
+            Color = color;
+            if (color == Player.White)
+            {
+                forward = Direction.North;
+            }
+            else if (color == Player.Black)
+            {
+                forward = Direction.South;
+            }
+        }
+        public override Pieces copy()
+        {
+            Pawn copy=new Pawn(Color);
+            copy.Move = Move;
+            return copy;
+        }
+        private static bool CanMoveTo(Position pos,Board board)
+        {
+            return Board.Inside(pos) && board.Empty(pos);
+        }
+
+        private bool CanCapture(Position pos, Board board)
+        {
+            if(!Board.Inside(pos) || board.Empty(pos))
+            {
+                return false;
+            }
+            return board[pos].Color != Color;
+        }
+
+        private IEnumerable<Base> ForwardMove(Position from, Board board)
+        {
+            Position onemove = from + forward;
+
+            if(CanMoveTo(onemove, board))
+            {
+                yield return new Normal(from, onemove);
+
+                Position twoMove=onemove + forward;
+                if(!Move && CanMoveTo(twoMove, board))
+                {
+                    yield return new Normal(from,twoMove);
+                }
+            }
+
+        }
+        private IEnumerable<Base> DiagonalMove(Position from, Board board) 
+        { 
+            foreach(Direction dir in new Direction[] {Direction.West, Direction.East})
+            {
+                Position to = from + forward + dir;
+
+                if(CanCapture(to,board))
+                {
+                    yield return new Normal(from, to);
+
+                }
+            }
+        }
+        public override IEnumerable<Base> Get (Position from, Board board)
+        {
+            return ForwardMove(from, board).Concat(DiagonalMove(from, board));
+        }
+
+        public override bool CanCaptureKing(Position from, Board board)
+        {
+            return DiagonalMove(from, board).Any(move =>
+            {
+                Pieces piece = board[move.To];
+                return piece != null && piece.type == PieceType.King;
+            });
+        }
+    }
+}
